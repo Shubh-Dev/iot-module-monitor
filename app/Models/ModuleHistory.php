4f796311d1e586201243bc6fbc0430987492ed8b
Shubh M Das <shubh.scb@gmail.com>
2024-11-07 10:57:02 +0000

    feat: ModuleHistory create

--- app/Models/ModuleHistory.php
+++ app/Models/ModuleHistory.php
@@ -2,9 +2,25 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ModuleHistory extends Model
 {
-    //
+    use HasFactory;
+    protected $table = 'module_history';
+
+    protected $fillable = [
+        'module_id', 
+        'measured_value', 
+        'status', 
+        'operating_time', 
+        'data_sent_count', 
+        'recorded_at'
+    ];
+
+    public function module()
+    {
+        return $this->belongsTo(Module::class);
+    }
 }
